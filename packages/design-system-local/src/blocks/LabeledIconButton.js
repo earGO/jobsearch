3cafec2aac4d98c0727795bb4e78f11d4db4f77a
Logvinov_fv <logvinov_fv@ursip.ru>
2019-08-01 17:11:23 +0300

    replaced datepicker with GOOD ONE #39

---
+++ packages/design-system-local/src/blocks/LabeledIconButton.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import theme from '../theme'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import {Icon, Text, Flex} from '../index'
+
+const Container = styled(Flex)`
+  cursor: pointer;
+  transition: all;
+  transition-timing-function: ${theme.timingFunctions.easeInOut};
+  transition-duration: ${theme.duration.fast};
+  &:hover {
+    transform: scale(1.0005);
+    opacity: 0.75;
+  }
+`
+
+const LabeledIconButton = ({
+  iconName,
+  caption,
+  color,
+  size,
+  fontSize,
+  onClick,
+  ...props
+}) => {
+  return (
+    <Container onClick={onClick}>
+      <Icon name={iconName} color={color} size={size} />
+      <Flex flexDirection={'column'} justifyContent={'center'} {...props}>
+        <Text color={color} fontSize={fontSize}>
+          {caption}
+        </Text>
+      </Flex>
+    </Container>
+  )
+}
+
+LabeledIconButton.propTypes = {
+  iconName: PropTypes.string,
+  size: PropTypes.number,
+  color: PropTypes.string,
+  caption: PropTypes.string,
+  fontSize: PropTypes.number
+}
+
+LabeledIconButton.defaultProps = {
+  iconName: 'error',
+  size: 24,
+  color: 'black',
+  caption: 'Default text',
+  fontSize: 2
+}
+
+export default LabeledIconButton
