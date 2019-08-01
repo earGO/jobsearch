914943f097c3376cd968c045b7162b35f97ba7e7
Logvinov_fv <logvinov_fv@ursip.ru>
2019-08-01 13:41:31 +0300

    started fixes in render #39

--- packages/services/src/lk.js
+++ packages/services/src/lk.js
@@ -1,7 +1,7 @@
 import {requestsReducer} from 'redux-saga-requests';
 
 const name = '@bim-service-lk';
-
+/* Using global port to give access to PMs and others and stuff */
 const api = 'http://192.168.5.90:3421/projects';
 
 /* Types */
